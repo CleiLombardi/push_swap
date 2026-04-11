38b56572daab2c2c770f0a9bce891b39d51875fd
roblomba <clei.lombardi@hotmail.it>
2026-04-11 19:48:37 +0200

    Add files via upload

---
+++ pa_pb.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pa_pb.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roblomba <roblomba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/16 20:09:02 by roblomba          #+#    #+#             */
+/*   Updated: 2026/03/28 20:58:07 by roblomba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap_header.h"
+
+void	pa(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*temp;
+
+	if (!stack_b || !*stack_b)
+		return ;
+	temp = *stack_b;
+	*stack_b = temp->next;
+	temp->next = *stack_a;
+	*stack_a = temp;
+	write (1, "pa\n", 3);
+}
+
+void	pb(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*temp;
+
+	if (!stack_a || !*stack_a)
+		return ;
+	temp = *stack_a;
+	*stack_a = temp->next;
+	temp->next = *stack_b;
+	*stack_b = temp;
+	write (1, "pb\n", 3);
+}
